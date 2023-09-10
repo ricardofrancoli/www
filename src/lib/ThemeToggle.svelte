bdda9c0742ff3bd858686607a5e3e3a26fe50e09
Ricardo Francoli <francoliricardo@gmail.com>
2023-09-10 22:46:37 +0200

    feat: set default colour theme, fetch from sessionStorage

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
-	let darkMode = true;
+	import { browser } from '$app/environment';
 
-	const switchDarkMode = () => {
-		darkMode = !darkMode;
+	const getIsDarkMode = () => {
+		if (import.meta.env.SSR) {
+			return false;
+		}
 
-		darkMode
+		const sessionData = window.sessionStorage.getItem('theme');
+
+		if (sessionData) {
+			return sessionData === 'dark';
+		}
+
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	};
+
+	export let isDarkMode = getIsDarkMode();
+
+	export const setDocumentClassList = () => {
+		isDarkMode
 			? document.documentElement.classList.add('dark')
 			: document.documentElement.classList.remove('dark');
-
-		console.log('document', document.documentElement.classList);
 	};
+
+	const switchDarkMode = () => {
+		isDarkMode = !isDarkMode;
+
+		setDocumentClassList();
+
+		sessionStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+	};
+
+	if (browser) {
+		setDocumentClassList();
+	}
 </script>
 
+<svelte:head>
+	<script>
+		const darkMode = sessionStorage.theme === 'dark';
+
+		if (darkMode) {
+			document.documentElement.classList.add('dark');
+		}
+	</script>
+</svelte:head>
+
 <div>
-	<input type="checkbox" id="theme-toggle" checked={darkMode} on:click={switchDarkMode} />
+	<input type="checkbox" id="theme-toggle" checked={isDarkMode} on:click={switchDarkMode} />
 	<label for="theme-toggle" />
 </div>
 
