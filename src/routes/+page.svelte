8e6c130657a7de8c4127be68a2881bd6552b2526
Ricardo Francoli <francoliricardo@gmail.com>
2023-09-22 16:02:23 +0200

    feat: basic header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="outer-container">
+	<div class="header-container">
+		<h1 class="main-header">Hi <span>👋</span> I'm Ricardo.</h1>
+		<h1 class="secondary-header">A <span class="web-dev-role">web developer</span>.</h1>
+	</div>
+</div>
 
 <style lang="postcss">
+	@import '../styles/variables.css';
+
+	.outer-container {
+		width: 100%;
+		display: flex;
+	}
+
+	.header-container {
+		width: fit-content;
+		height: 50vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		h1 {
+			margin: 0;
+			line-height: 95%;
+			font-weight: 600;
+		}
+
+		.web-dev-role {
+			color: var(--primary-colour);
+		}
+	}
+
+	@media (--md) {
+		.outer-container {
+			justify-content: center;
+		}
+
+		.header-container {
+			height: 100vh;
+		}
+
+		h1 {
+			line-height: 90%;
+			font-size: 3rem;
+		}
+	}
+
+	@media (--lg) {
+		h1 {
+			font-size: 5rem;
+		}
+	}
 </style>
