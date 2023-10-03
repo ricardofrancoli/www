5c995d626d1b81d8eba3fe0a26ace3192712741c
Ricardo Francoli <francoliricardo@gmail.com>
2023-10-03 15:14:56 +0200

    feat(project): layout for md screens

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -4,17 +4,19 @@
 	export let project: Project;
 </script>
 
-<div class="project">
-	<h3>{project.title}</h3>
-	<div class="project-screenshot-container">
-		<img class="project-screenshot" src={project.screenshot} alt="" />
-	</div>
-	<p>{project.description}</p>
-	<div class="project-links">
-		{#if project.links.live}
-			<a href={project.links.live}>See live</a>
-		{/if}
-		<a href={project.links.github}>Github</a>
+<div class="project-container">
+	<div class="project">
+		<h3>{project.title}</h3>
+		<div class="project-screenshot-container">
+			<img class="project-screenshot" src={project.screenshot} alt="" />
+		</div>
+		<p>{project.description}</p>
+		<div class="project-links">
+			{#if project.links.live}
+				<a href={project.links.live}>See live</a>
+			{/if}
+			<a href={project.links.github}>Github</a>
+		</div>
 	</div>
 </div>
 
@@ -31,6 +33,20 @@
 		p {
 			margin: 0;
 		}
+
+		@media (--md) {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			grid-template-rows: auto 1fr auto;
+
+			.project-screenshot-container {
+				grid-row: 1/4;
+			}
+
+			p {
+				min-height: 10rem;
+			}
+		}
 	}
 
 	.project-screenshot-container {
@@ -44,6 +60,16 @@
 			max-height: 90%;
 			width: auto;
 			height: auto;
+
+			@media (--md) {
+				margin-top: auto;
+			}
+		}
+	}
+
+	@media (--md) {
+		.project-container {
+			height: 20rem;
 		}
 	}
 </style>
