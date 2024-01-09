bb4939bae0ab147c6987aaa6b99f868f8c0e823a
Ricardo Francoli <francoliricardo@gmail.com>
2024-01-09 11:44:04 +0100

    feat(chore): add autoprefixer

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,7 @@ const atImport = require('postcss-import');
 const postcssMixins = require('postcss-mixins');
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssCustomMedia = require('postcss-custom-media');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
 	plugins: [
@@ -17,7 +18,7 @@ module.exports = {
 				'media-query-ranges': true
 			}
 		}),
-		postcssCustomMedia()
-		// TODO: add autoprefixer?
+		postcssCustomMedia(),
+		autoprefixer()
 	]
 };
